3b9777f9ad0a4a078f18fbb14937bd176d67f203
mago_d_Mercurio <davila.feria@gmail.com>
2023-05-03 16:24:37 -0500

    add stepper component

--- src/Form/index.jsx
+++ src/Form/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Typography } from "@mui/material";
 import { LogoSpace, MainSpace, FormSpace, Img } from "./styles";
 import DatosUsuario from "./DatosUsuario";
@@ -8,6 +8,15 @@ import Complete from "./Complete";
 import Stepper from "../Stepper";
 
 const Form = () => {
+  const [step, setStep] = useState(0)
+
+    const steps = {
+    0: <DatosUsuario />,
+    1: <DatosPersonales />,
+    2: <DatosEntrega />, 
+    3: <Complete />
+  }
+  
   return (
     <Box
       sx={{
@@ -21,9 +30,12 @@ const Form = () => {
         <Typography variant="h3">AluraFood</Typography>
       </LogoSpace>
       <FormSpace>
-        <DatosUsuario />
+        { step < 3 && <Stepper step={ step } />}
+        {/* <DatosUsuario />
         <DatosPersonales />
-        <DatosEntrega />
+        <DatosEntrega /> */}
+
+        { steps[step] }
       </FormSpace>
     </Box>
   );
