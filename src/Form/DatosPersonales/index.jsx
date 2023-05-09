7b3cd56fd12ffd61d84e49afd893d3311db000f5
mago_d_Mercurio <davila.feria@gmail.com>
2023-05-09 14:02:12 -0500

    validacion de datosPersonales

--- src/Form/DatosPersonales/index.jsx
+++ src/Form/DatosPersonales/index.jsx
@@ -1,7 +1,14 @@
-import React from "react";
-import { TextField, Button, Box } from "@mui/material";
+import React, { useState } from "react"
+import { TextField, Button, Box } from "@mui/material"
+import { validarNombre, validarApellidos, validarTelefono } from "./validaciones"
 
 const DatosPersonales = ({updateStep}) => {
+
+  const [name, setName] = useState({ value: '', valid: null})
+  const [lastName, setLastName] = useState({ value: '', valid: null})
+  const [phone, setPhone] = useState({ value: '', valid: null});
+
+
   return (
     <Box
       component="form"
@@ -25,6 +32,13 @@ const DatosPersonales = ({updateStep}) => {
         fullWidth
         margin="dense"
         type="text"
+        value={name.value}
+        onChange={ (input) => {
+          const value = input.target.value
+          const valid = validarNombre(value)
+          setName({ value, valid})
+          console.log(value, valid)
+        }}
       />
       <TextField
         label="Apellidos"
@@ -32,6 +46,12 @@ const DatosPersonales = ({updateStep}) => {
         fullWidth
         margin="dense"
         type="text"
+        value={lastName.value}
+        onChange={ (input) => {
+          const value = input.target.value
+          const valid = validarApellidos(value)
+          setLastName({ value, valid})
+        }}
       />
       <TextField
         label="Número telefónico"
@@ -40,6 +60,12 @@ const DatosPersonales = ({updateStep}) => {
         margin="dense"
         type="number"
         inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+        value={phone.value}
+        onChange={ (input) => {
+          const value = input.target.value
+          const valid = validarTelefono(value)
+          setLastName({ value, valid})
+        }}
       />
       <Button variant="contained" type="submit">
         Siguiente
