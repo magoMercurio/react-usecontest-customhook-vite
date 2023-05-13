9e3b1f66651d81ad4f2599779138b6779d7b673e
mago_d_Mercurio <davila.feria@gmail.com>
2023-05-12 22:02:39 -0500

    add Step component

--- src/Form/index.jsx
+++ src/Form/index.jsx
@@ -1,12 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 import { Box, Typography } from "@mui/material";
-import { LogoSpace, MainSpace, FormSpace, Img } from "./styles";
+import { LogoSpace, FormSpace, Img } from "./styles";
 import DatosUsuario from "./DatosUsuario";
 import DatosPersonales from "./DatosPersonales";
 import DatosEntrega from "./DatosEntrega";
 import Complete from "./Complete";
 import Stepper from "../Stepper";
+import Step from "./Step";
+
+//Validaciones
+import { validarEmail, validarPassword } from "./DatosUsuario/validaciones";
 
 const Form = () => {
   const [step, setStep] = useState(0)
@@ -21,6 +25,46 @@ const Form = () => {
     1: <DatosPersonales updateStep={updateStep} />,
     2: <DatosEntrega updateStep={updateStep} />, 
     3: <Complete />
+  }
+
+  const onSubmit = () => {}
+
+  const handleChange = (element, position, currentStep, validator) => {
+    const value = element.target.value
+    const valid = validator(value)
+    console.log(value)
+    console.log(valid)
+    console.log('position:', position)
+    console.log('currentStep:', currentStep)
+    console.log('validator:', validator)
+
+  }
+  
+  const stepFlow = { 
+    0: {
+      inputs: [
+        {
+          label: 'correo electronico',
+          type: 'email',
+          value: '',
+          valid: null,
+          onChange: handleChange,
+          helperText: 'Ingresar un correo valido',
+          validator: validarEmail,
+        },
+        {
+          label: 'contraseña',
+          type: 'password',
+          value: '',
+          valid: null,
+          onChange: handleChange,
+          helperText: 'Ingresa una contraseña valida, Al menos 8 caracteres y maximo 20.',
+          validator: validarPassword
+        }
+      ],
+      buttonText: 'Siguiente',
+      onSubmit,
+    },
   }
   
   return (
@@ -37,14 +81,12 @@ const Form = () => {
       </LogoSpace>
       <FormSpace>
         { step < 3 && <Stepper step={ step } />}
-        {/* <DatosUsuario />
-        <DatosPersonales />
-        <DatosEntrega /> */}
-
-        { steps[step] }
+      {/*   { steps[step] } */}
+      <Step data={ stepFlow[step] } step={step} />
       </FormSpace>
     </Box>
   );
 };
 
 export default Form;
+``
